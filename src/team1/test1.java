f61e3d95826941c72b32a5e61dc35ec0db809eea
soldesk <soldesk@DESKTOP-T1BMDAM>
2023-02-03 11:07:23 +0900

    0203_2

--- src/team1/test1.java
+++ src/team1/test1.java
@@ -3,6 +3,22 @@ package team1;
 public class test1 {
 	
 	int grade;
+	String name;
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+		
 	
 
 }
